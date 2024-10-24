5cef5190018afc20eb0bd955717d3a6a84665702
Lucía Bonilla <lucia.bonilla@correo.ucu.edu.uy>
2024-10-24 17:45:36 -0300

    Dockerización inicial

---
+++ src/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, jsonify
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+# Obtén las variables de entorno para la base de datos, definidas en docker-compose.yml
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
+
+# Ruta principal.
+@app.route('/')
+def index():
+    return jsonify(message="Deportes de nieve de la UCU", db_host=DB_HOST, db_port=DB_PORT, db_user=DB_USER, db_password=DB_PASSWORD, db_name=DB_NAME)
+
+@app.route('/db-check', methods=['GET'])
+def db_check():
+    try:
+        # Intenta conectarte a la base de datos usando las variables de entorno
+        connection = mysql.connector.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+
+        if connection.is_connected():
+            return jsonify({"message": "Conexión exitosa a la base de datos"}), 200
+        else:
+            return jsonify({"message": "Error al conectar con la base de datos"}), 500
+    except mysql.connector.Error as err:
+        # Captura los errores de conexión y muestra un mensaje útil
+        return jsonify({"message": f"Error al conectar a la base de datos: {err}"}), 500
+    finally:
+        # Asegúrate de cerrar la conexión si fue exitosa
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
